b69aeeb3fd78977674ac2821e7e1eb0a91d6e055
akramsameer <akramsameer99@gmail.com>
2018-04-16 20:06:45 +0200

    ddddddadad

---
+++ ChairtyApp/Models/ViewModels/RequestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChairtyApp.Models.ViewModels
+{
+    public class RequestViewModel
+    {
+        public string ProbelmStatment { get; set; }
+        public string  S { get; set; }
+    }
+}
