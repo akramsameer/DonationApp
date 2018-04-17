095bdec877aa2ce713297c807eba6018f5bda198
akramsameer <akramsameer99@gmail.com>
2018-04-17 02:03:29 +0200

    Finish Vedio Page

---
+++ ChairtyApp/Models/ViewModels/VedioViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChairtyApp.Models.ViewModels
+{
+    public class VedioViewModel
+    {
+        public vediosTbl Vedio { get; set; }
+        
+        public List<vediosTbl> ListOfVedios;
+
+
+    }
+}
